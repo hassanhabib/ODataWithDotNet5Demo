b13e05230ff57a2202e788582d4f811270353731
Hassan Habib <hassanhabib@live.com>
2020-10-09 23:35:59 -0700

    Add project files.

---
+++ ODataWithDotNet5Demo/Controllers/StudentsController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNet.OData;
+using Microsoft.AspNetCore.Mvc;
+using ODataWithDotNet5Demo.Models;
+
+namespace ODataWithDotNet5Demo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        [HttpGet]
+        [EnableQuery]
+        public ActionResult<List<Student>> GetAllStudents()
+        {
+            return Ok(new List<Student>
+            {
+                new Student
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Viral Bahen",
+                    Grade = 100
+                },
+                new Student
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Josh McCall",
+                    Grade = 150
+                },
+                new Student
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Kailu Hu",
+                    Grade = 90
+                }
+            });
+        }
+    }
+}
---
+++ ODataWithDotNet5Demo/Models/Student.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ODataWithDotNet5Demo.Models
+{
+    public class Student
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public int Grade { get; set; }
+    }
+}
